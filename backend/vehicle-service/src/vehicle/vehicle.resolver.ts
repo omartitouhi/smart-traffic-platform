c5a755de971dc83d336e2c523e12533e728ae6ca
Omar Titouhi <titouhiomar223@gmail.com>
2026-06-02 01:06:49 +0100

    feat(vehicle): add vehicle resolver with queries and mutations for vehicle management

---
+++ backend/vehicle-service/src/vehicle/vehicle.resolver.ts
@@ -0,0 +1,111 @@
+import {
+  Args,
+  Field,
+  Float,
+  ID,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from '@nestjs/graphql';
+import { AddVehiclePositionInput } from './dto/add-vehicle-position.input';
+import { CreateVehicleInput } from './dto/create-vehicle.input';
+import { UpdateVehicleInput } from './dto/update-vehicle.input';
+import { VehicleService } from './vehicle.service';
+
+@ObjectType()
+class VehicleEntity {
+  @Field(() => ID)
+  id!: string;
+
+  @Field()
+  matricule!: string;
+
+  @Field()
+  brand!: string;
+
+  @Field()
+  model!: string;
+
+  @Field()
+  type!: string;
+
+  @Field()
+  status!: string;
+
+  @Field()
+  createdAt!: Date;
+
+  @Field()
+  updatedAt!: Date;
+}
+
+@ObjectType()
+class VehiclePositionEntity {
+  @Field(() => ID)
+  id!: string;
+
+  @Field(() => ID)
+  vehicleId!: string;
+
+  @Field(() => Float)
+  latitude!: number;
+
+  @Field(() => Float)
+  longitude!: number;
+
+  @Field(() => Float)
+  speed!: number;
+
+  @Field()
+  recordedAt!: Date;
+}
+
+@Resolver(() => VehicleEntity)
+export class VehicleResolver {
+  constructor(private readonly vehicleService: VehicleService) {}
+
+  @Query(() => [VehicleEntity])
+  vehicles(): Promise<VehicleEntity[]> {
+    return this.vehicleService.getVehicles();
+  }
+
+  @Query(() => VehicleEntity)
+  vehicle(@Args('id', { type: () => ID }) id: string): Promise<VehicleEntity> {
+    return this.vehicleService.getVehicleById(id);
+  }
+
+  @Query(() => [VehiclePositionEntity])
+  vehiclePositions(
+    @Args('vehicleId', { type: () => ID }) vehicleId: string,
+  ): Promise<VehiclePositionEntity[]> {
+    return this.vehicleService.getPositionHistory(vehicleId);
+  }
+
+  @Mutation(() => VehicleEntity)
+  createVehicle(
+    @Args('input') input: CreateVehicleInput,
+  ): Promise<VehicleEntity> {
+    return this.vehicleService.createVehicle(input);
+  }
+
+  @Mutation(() => VehicleEntity)
+  updateVehicle(
+    @Args('id', { type: () => ID }) id: string,
+    @Args('input') input: UpdateVehicleInput,
+  ): Promise<VehicleEntity> {
+    return this.vehicleService.updateVehicle(id, input);
+  }
+
+  @Mutation(() => Boolean)
+  deleteVehicle(@Args('id', { type: () => ID }) id: string): Promise<boolean> {
+    return this.vehicleService.deleteVehicle(id);
+  }
+
+  @Mutation(() => VehiclePositionEntity)
+  addVehiclePosition(
+    @Args('input') input: AddVehiclePositionInput,
+  ): Promise<VehiclePositionEntity> {
+    return this.vehicleService.addPosition(input);
+  }
+}
